f4dda2b6dcb072dcf6018302519ac50f76349863
kedroboroni <svishoyrik@mail.ru>
2024-08-23 22:05:52 +0300

    написал новую идею про регистрацию.

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ with open("tokenbot.json", "r", encoding = "UTF-8") as file:
 bot = telebot.TeleBot(token)
 
 
-
 @bot.message_handler(commands=["start"])
 def greeting(message):
+
 
     bot.send_message(message.chat.id, "Добавим json формат") #!!!Отредактировать файл json для вывода текста!
     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
@@ -23,7 +23,7 @@ def greeting(message):
     markup.add(sexButtonM, sexButtonW)
     bot.send_message(message.chat.id, "Выберите опцию в меню.", reply_markup=markup)
     bot.register_next_step_handler(message, handle_option_choice)
-
+#Добавить проверку для того регистрировался данны человек или нет. через БД, что бы при попвторном вызове функции старт он не выбирал пол, но мог перерегистроваться
 
 
 def handle_option_choice(message):
@@ -33,6 +33,7 @@ def handle_option_choice(message):
         # Обработка выбора опции
         bot.send_message(message.chat.id, f"Вы выбрали: {message.text}, сохраняю ваш выбор в совбю БД", reply_markup=types.ReplyKeyboardRemove())
         bot.register_next_step_handler(message, nextStepForRegister)
+        
     else:
 
         # Повторное предложение опций
